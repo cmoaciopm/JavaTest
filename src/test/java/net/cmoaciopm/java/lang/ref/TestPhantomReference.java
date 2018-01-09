6bd85755763dd7aa556ed1f0e1788b32cb0fe0f8
gonganmin <cmoaciopm@gmail.com>
2018-01-09 22:50:20 +0800

    Fix indent issue

--- src/test/java/net/cmoaciopm/java/lang/ref/TestPhantomReference.java
+++ src/test/java/net/cmoaciopm/java/lang/ref/TestPhantomReference.java
@@ -13,20 +13,20 @@ import static org.junit.Assert.assertThat;
 
 public class TestPhantomReference {
 
-   @Test(expected = NoSuchFieldException.class)
-   public void test() throws InterruptedException, NoSuchFieldException {
-      ReferenceQueue<Integer> queue = new ReferenceQueue<>();
-      PhantomReference<Integer> phantomReference = new PhantomReference<>(new Integer(999), queue);
-      assertThat(phantomReference.get(), is(nullValue()));
-      assertThat(phantomReference.isEnqueued(), is(false));
-      System.gc();
-      // Wait for gc
-      Thread.sleep(1000);
-      assertThat(phantomReference.isEnqueued(), is(true));
-      Reference reference;
-      while ((reference = queue.remove()) != null) {
-         assertThat(reference.get(), is(nullValue()));
-         reference.getClass().getDeclaredField("referent");
-      }
-   }
+    @Test(expected = NoSuchFieldException.class)
+    public void test() throws InterruptedException, NoSuchFieldException {
+        ReferenceQueue<Integer> queue = new ReferenceQueue<>();
+        PhantomReference<Integer> phantomReference = new PhantomReference<>(new Integer(999), queue);
+        assertThat(phantomReference.get(), is(nullValue()));
+        assertThat(phantomReference.isEnqueued(), is(false));
+        System.gc();
+        // Wait for gc
+        Thread.sleep(1000);
+        assertThat(phantomReference.isEnqueued(), is(true));
+        Reference reference;
+        while ((reference = queue.remove()) != null) {
+            assertThat(reference.get(), is(nullValue()));
+            reference.getClass().getDeclaredField("referent");
+        }
+    }
 }
--- src/test/java/net/cmoaciopm/java/lang/ref/TestWeakReference.java
+++ src/test/java/net/cmoaciopm/java/lang/ref/TestWeakReference.java
@@ -12,28 +12,28 @@ import static org.junit.Assert.assertThat;
 
 public class TestWeakReference {
 
-   private static final int M = 1024 * 1024;
+    private static final int M = 1024 * 1024;
 
-   @Test
-   public void test() {
-      Runtime runtime = Runtime.getRuntime();
-      long freeMemory = runtime.freeMemory()/M;
+    @Test
+    public void test() {
+        Runtime runtime = Runtime.getRuntime();
+        long freeMemory = runtime.freeMemory() / M;
 
-      WeakReference<Object> weakReference = new WeakReference<>(new byte[10 * M]);
-      long freeMemory2 = runtime.freeMemory()/M;
-      assertThat(freeMemory - freeMemory2, is(greaterThanOrEqualTo(10L)));
+        WeakReference<Object> weakReference = new WeakReference<>(new byte[10 * M]);
+        long freeMemory2 = runtime.freeMemory() / M;
+        assertThat(freeMemory - freeMemory2, is(greaterThanOrEqualTo(10L)));
 
-      System.gc();
+        System.gc();
 
-      long freeMemory3 = runtime.freeMemory()/M;
-      assertThat(freeMemory3, greaterThanOrEqualTo(freeMemory));
-   }
+        long freeMemory3 = runtime.freeMemory() / M;
+        assertThat(freeMemory3, greaterThanOrEqualTo(freeMemory));
+    }
 
-   @Test
-   public void testGet() throws InterruptedException {
-      WeakReference<Object> weakReference = new WeakReference<>(new byte[10 * M]);
-      assertThat(weakReference.get(), is(notNullValue()));
-      System.gc();
-      assertThat(weakReference.get(), is(nullValue()));
-   }
+    @Test
+    public void testGet() throws InterruptedException {
+        WeakReference<Object> weakReference = new WeakReference<>(new byte[10 * M]);
+        assertThat(weakReference.get(), is(notNullValue()));
+        System.gc();
+        assertThat(weakReference.get(), is(nullValue()));
+    }
 }
